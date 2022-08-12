d5dade4ec039b01b1b9f87f5872f2d763403c5ba
kathrynsayer <kssayer@gmail.com>
2022-08-11 17:00:20 -0700

    finished javascript exercise

---
+++ app.js
@@ -0,0 +1,18 @@
+const firstName = "Kathryn";
+let lastName = "Sayer";
+var age = "36";
+let language = "JavaScript";
+let createdYear = "1995";
+let isCaseSensitive = "true";
+let price = "19.99";
+let isOnSale = "false";
+let salePercentage = "15";
+let stock = "0";
+let inStock = "false";
+let selectedSize = "M";
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = "722";
+let bookmark = "456";
+let hasRead = "true"; 
+console.log("Hello World!")
